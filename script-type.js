fbacaef9048a1651f07ed239d254e20a367d8b4c
dmassalif <141333144+dmassalif@users.noreply.github.com>
2023-08-19 20:06:24 +0800

    Add files via upload

---
+++ script-type.js
@@ -0,0 +1,12 @@
+// typed text //
+
+/*=== Typed Reveal ===*/
+const typed = new Typed('.multiple-text', {
+    strings: ['Fronted Newbie','Student','Designer'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+    });
+    
+    /*=== end ===*/
